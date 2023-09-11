198ce68eb6256934c70c8dcdec1e4e10bb908b22
siyavash <feyzisiyavash@gmail.com>
2023-09-11 17:00:47 +0330

    fix bug

--- todolist/models.py
+++ todolist/models.py
@@ -7,6 +7,5 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
 
-
-def __str__(self):
-    return self.title()
+    def __str__(self):
+        return self.title()
