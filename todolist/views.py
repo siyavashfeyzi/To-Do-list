4c2b5b5a2f0e8687ea74d78c984b4e97c2de0b02
siyavash <feyzisiyavash@gmail.com>
2023-09-12 19:21:28 +0330

    V0.1

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from . models import Task
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todolist/task_list.html', {'tasks': tasks})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title', '')
+        Task.objects.create(title=title)
+        return redirect('task_list')
+
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
