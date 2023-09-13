13c4871c33249b3c8fe04a4fc71c5bdb2e5f4782
siyavash <feyzisiyavash@gmail.com>
2023-09-13 16:48:02 +0330

    Add Postgresql cofig

--- todolist_project/settings.py
+++ todolist_project/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'todolist_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ToDolist',
+        'USER': 'postgres',
+        'PASSWORD': 'youcandoit',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
