315d5b7a8d0182500ff5c798037c3da001431b57
siyavash <feyzisiyavash@gmail.com>
2023-09-19 16:55:25 +0330

    Add description, create_at, last_update field to Tak

--- todolist/models.py
+++ todolist/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
+    description = models.TextField(null=True)
     completed = models.BooleanField(default=False)
+    create_at = models.DateTimeField(default=timezone.now)
+    last_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title()
+        return self.title
