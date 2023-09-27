5c55e8b1c0926bb6d737d459f241a1d289fc835d
siyavash <feyzisiyavash@gmail.com>
2023-09-27 10:49:08 +0330

    set the ALLOWED_HOSTS in production mode

--- todolist_project/settings.py
+++ todolist_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f^mi71(av*swotq0o)3slxs)@2y#*(__)p^btp&afu8+4qd=*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
