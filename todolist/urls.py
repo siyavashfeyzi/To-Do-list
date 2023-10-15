763d908b6df2e0214c6d99df6cfe0e4d9c849b1d
siyavash <feyzisiyavash@gmail.com>
2023-10-15 15:56:35 +0330

    Add update urls

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add_task/', views.add_task, name='add_task'),
+    path('update_task/<int:task_id>/', views.update_task, name='update_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
