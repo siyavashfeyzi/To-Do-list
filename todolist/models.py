03f61303a4dd616ec2283a99c664da9c280dbaf9
siyavash <feyzisiyavash@gmail.com>
2023-12-02 16:31:55 +0330

    Add priority with choices

--- todolist/models.py
+++ todolist/models.py
@@ -5,11 +5,21 @@ from django.utils import timezone
 
 
 class Task(models.Model):
+    PRIORITY_LOW = 'L'
+    PRIORITY_MEDIUM = 'M'
+    PRIORITY_TOP = 'T'
+
+    PRIORITY_CHOICES = [
+        (PRIORITY_LOW, 'Low'),
+        (PRIORITY_MEDIUM, 'Medium'),
+        (PRIORITY_TOP, 'Top'),
+    ]
     title = models.CharField(max_length=200)
     description = models.TextField(null=True)
     completed = models.BooleanField(default=False)
     create_at = models.DateTimeField(default=timezone.now)
     last_update = models.DateTimeField(auto_now=True)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES)
 
     def __str__(self):
         return self.title
